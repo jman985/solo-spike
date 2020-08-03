fe3f856ca901b586655d06a13f0a75404aae4703
Josh Maus <jman985@gmail.com>
2020-08-02 22:40:43 -0500

    added flip material UI

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { connect } from 'react-redux';
-import { MDBCard, MDBCardTitle, MDBBtn, MDBRow, MDBCol, MDBIcon } from 'mdbreact';
-import Box from '@material-ui/core/Box';
+import { MDBCard, MDBCardTitle, MDBBtn, MDBRow, MDBCol, MDBIcon, MDBCardBody, MDBCardImage, MDBCardText} from 'mdbreact';
 import { borders } from '@material-ui/system';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import CardHeader from '@material-ui/core/CardHeader';
+import Collapse from '@material-ui/core/Collapse';
+import { Box, Grid, Slide, Paper, Typography} from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import ReactCardFlip from 'react-card-flip';
 
-const defaultProps = {
-  bgcolor: 'background.paper',
-  borderColor: 'text.primary',
-  m: 1,
-  border: 1,
-  style: { width: '5rem', height: '5rem' },
-};
+
+class App extends Component {
 
 
-const CardExample = () => {
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  constructor() {
+    super();
+      this.state = {
+      isFlipped: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+ 
+  handleClick(e) {
+    e.preventDefault();
+    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+  }
+
+  render(){
   return (
+    <>
+    <Router>
     <MDBRow container spacing = {50}>
       <MDBCol md='3'>
         
-        <MDBCard
+        <MDBCard testimonial
         className='card-image'
 
           style={{
@@ -49,16 +74,17 @@ const CardExample = () => {
           </div>
         </MDBCard>
       </MDBCol>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
       <MDBCol md='3'>
         <MDBCard
           className='card-image'
           style={{
             backgroundImage:
-              "url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Intel-logo.svg/610px-Intel-logo.svg.png')"
+              "url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Intel-logo.svg/500px-Intel-logo.svg.png')"
           }}
         >
-          <div className='text-white text-center d-flex align-items-center rgba-blue-strong py-3 px-2'>
+          <div className='text-white text-center d-flex align-items-center rgba-blue-strong py-4 px-2'>
             <div>
               <MDBCardTitle tag='h3' className='pt-4'>
                 <strong>Intel</strong>
@@ -79,11 +105,59 @@ const CardExample = () => {
           </div>
         </MDBCard>
       </MDBCol>
+      <MDBCol md="3">
+
+      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+        <MDBCard >
+          <MDBCardImage
+            className='img-fluid'
+            overlay="white-light"
+            src='https://images.anandtech.com/doci/14735/AMD_Radeon_Car_678x452.png'
+          />
+          <MDBBtn
+            className='ml-auto mr-4 lighten-3 mdb-coalor' onClick= {this.handleClick}          
+          >Details&nbsp;&nbsp;
+            <MDBIcon icon='plus'/>
+          </MDBBtn>
+          <MDBCardBody >
+          
+          </MDBCardBody>
+        </MDBCard>
+        <MDBCard >
+          <MDBCardImage
+            className='img-fluid'
+            overlay="white-light"
+            src='https://images.anandtech.com/doci/14735/AMD_Radeon_Car_678x452.png'
+          />
+          <MDBCardBody >
+                <p><strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit</strong></p>
+          </MDBCardBody>
+          <MDBBtn
+            className='ml-auto mr-4 lighten-3 mdb-coalor' onClick= {this.handleClick}          
+          >Add Hardware&nbsp;&nbsp;
+            <MDBIcon icon='plus'/>
+            </MDBBtn>
+          </MDBCard>
+        </ReactCardFlip>
+
+      </MDBCol>
     </MDBRow>
-
-
-
+    </Router>
+{/* <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+        <Card>
+          This is the front of the card.
+          <button onClick={this.handleClick}>Click to flip</button>
+        </Card>
+ 
+        <Button>
+          This is the back of the card.
+          <button onClick={this.handleClick}>Click to flip</button>
+        </Button>
+      </ReactCardFlip> */}
+</>
   )
 }
+}
 
-export default CardExample;
+export default App;
